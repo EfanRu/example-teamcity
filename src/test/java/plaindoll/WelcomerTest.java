ab88eb21198c240353317552325e3d4f541b9dc4
Slava E <ffgeniy@mail.ru>
2023-04-11 23:30:33 +0300

    Revert "Add sayWtf method."
    This reverts commit 8b755bd9aedde60230d255b52744eae966b8e2f1.

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -30,8 +30,4 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
-	@Test
-	public void welcomerSaysWtf() {
-		assertThat(welcomer.sayWtf(), containsString("Hunter"));
-	}
 }
